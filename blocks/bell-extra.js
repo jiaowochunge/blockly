c4ced38bd702fbfb16f5ec76c8bbd46597c00b78
tinychou <tinychou@tinys-MacBook-Pro.local>
2017-04-12 10:31:52 +0800

    add all blocks for extra category from pc ide.

--- blocks/bell-extra.js
+++ blocks/bell-extra.js
@@ -5,31 +5,568 @@ goog.require('Blockly.Blocks');
 
 Blockly.Blocks.bell_extra.HUE = '#123456';
 
-Blockly.Blocks['bell_extra_get_touch_state'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.BELL_EXTRA_GET_TOUCH_TITLE)
-        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"], ["14","14"], ["15","15"]]), "sequence_name");
-    this.setInputsInline(true);
-    this.setOutput(true, "Boolean");
-    this.setColour(Blockly.Blocks.bell_extra.HUE);
-    this.setTooltip(Blockly.Msg.BELL_EXTRA_GET_TOUCH_TOOLTIP);
-    // TODO
-    this.setHelpUrl('../file_path_to_help.html');
-  }
+Blockly.Blocks['bell_single_motor_light'] = {
+  init: function () {
+    this.jsonInit(
+      {
+        "type": "bell_single_motor_light",
+        "message0": Blockly.Msg.BELL_LIGHT_SINGLE_MOTOR_TITLE,
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "motor",
+            "options": [
+              [
+                "1",
+                "1"
+              ],
+              [
+                "2",
+                "2"
+              ],
+              [
+                "3",
+                "3"
+              ],
+              [
+                "4",
+                "4"
+              ],
+              [
+                "5",
+                "5"
+              ],
+              [
+                "6",
+                "6"
+              ],
+              [
+                "7",
+                "7"
+              ],
+              [
+                "8",
+                "8"
+              ],
+              [
+                "9",
+                "9"
+              ],
+              [
+                "10",
+                "10"
+              ],
+              [
+                "11",
+                "11"
+              ],
+              [
+                "12",
+                "12"
+              ],
+              [
+                "13",
+                "13"
+              ],
+              [
+                "14",
+                "14"
+              ],
+              [
+                "15",
+                "15"
+              ]
+            ]
+          },
+          {
+            "type": "field_colour",
+            "name": "colour",
+            "colour": "#f45d5d"
+          },
+          {
+            "type": "field_dropdown",
+            "name": "mode",
+            "options": [
+              [
+                Blockly.Msg.BELL_LIGHT_MODE_BREATHING,
+                "3"
+              ],
+              [
+                Blockly.Msg.BELL_LIGHT_MODE_FLASHING,
+                "2"
+              ],
+              [
+                Blockly.Msg.BELL_LIGHT_MODE_REGULAR,
+                "1"
+              ],
+              [
+                Blockly.Msg.BELL_LIGHT_MODE_NONE,
+                "0"
+              ]
+            ]
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": Blockly.Blocks.bell_light.HUE,
+        "tooltip": Blockly.Msg.BELL_LIGHT_SINGLE_MOTOR_TOOLTIP,
+        "helpUrl": Blockly.Msg.BELL_LIGHT_SINGLE_MOTOR_HELPURL
+      }
+    );
+    this.getField('colour')
+      .setColours(['#f45d5d','#0eef53','#f6f550','#4d9edc','#d451f8','#07edfb','#f29450','#fcfcfb'])
+      .setColumns(4);
+  },
 };
 
-Blockly.Blocks['bell_extra_set_hservo_angle'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.BELL_EXTRA_SET_HSERVO_TITLE)
-        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"], ["14","14"], ["15","15"]]), "sequence_name")
-        .appendField(new Blockly.FieldNumber(90, 0, 180, 1), "angle");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(Blockly.Blocks.bell_extra.HUE);
-    this.setTooltip(Blockly.Msg.BELL_EXTRA_SET_HSERVO_TOOLTIP);
-    // TODO
-    this.setHelpUrl('../file_path_to_help.html');
-  }
+Blockly.Blocks['bell_touch_state'] = {
+	init: function () {
+		this.jsonInit(
+			{
+			  "type": "bell_touch_state",
+			  "message0": Blockly.Msg.BELL_MOTION_TOUCH_STATE_TITLE,
+			  "args0": [
+			    {
+			      "type": "field_dropdown",
+			      "name": "sequence",
+			      "options": [
+			        [
+			          "1",
+			          "1"
+			        ],
+			        [
+			          "2",
+			          "2"
+			        ],
+			        [
+			          "3",
+			          "3"
+			        ],
+			        [
+			          "4",
+			          "4"
+			        ],
+			        [
+			          "5",
+			          "5"
+			        ],
+			        [
+			          "6",
+			          "6"
+			        ],
+			        [
+			          "7",
+			          "7"
+			        ],
+			        [
+			          "8",
+			          "8"
+			        ],
+			        [
+			          "9",
+			          "9"
+			        ],
+			        [
+			          "10",
+			          "10"
+			        ],
+			        [
+			          "11",
+			          "11"
+			        ],
+			        [
+			          "12",
+			          "12"
+			        ],
+			        [
+			          "13",
+			          "13"
+			        ],
+			        [
+			          "14",
+			          "14"
+			        ],
+			        [
+			          "15",
+			          "15"
+			        ]
+			      ]
+			    }
+			  ],
+				"output": "Boolean",
+				"colour": Blockly.Blocks.bell_motion.HUE,
+			  "tooltip": Blockly.Msg.BELL_MOTION_TOUCH_STATE_TOOLTIP,
+			  "helpUrl": Blockly.Msg.BELL_MOTION_TOUCH_STATE_HELPURL
+			});
+	},
+};
+
+Blockly.Blocks['bell_set_hservo_angle'] = {
+	init: function () {
+		this.jsonInit(
+			{
+			  "type": "bell_set_hservo_angle",
+			  "message0": Blockly.Msg.BELL_MOTION_SET_HSERVO_ANGLE_TITLE,
+			  "args0": [
+			    {
+			      "type": "field_dropdown",
+			      "name": "sequence",
+			      "options": [
+			        [
+			          "1",
+			          "1"
+			        ],
+			        [
+			          "2",
+			          "2"
+			        ],
+			        [
+			          "3",
+			          "3"
+			        ],
+			        [
+			          "4",
+			          "4"
+			        ],
+			        [
+			          "5",
+			          "5"
+			        ],
+			        [
+			          "6",
+			          "6"
+			        ],
+			        [
+			          "7",
+			          "7"
+			        ],
+			        [
+			          "8",
+			          "8"
+			        ],
+			        [
+			          "9",
+			          "9"
+			        ],
+			        [
+			          "10",
+			          "10"
+			        ],
+			        [
+			          "11",
+			          "11"
+			        ],
+			        [
+			          "12",
+			          "12"
+			        ],
+			        [
+			          "13",
+			          "13"
+			        ],
+			        [
+			          "14",
+			          "14"
+			        ],
+			        [
+			          "15",
+			          "15"
+			        ]
+			      ]
+			    },
+					{
+			      "type": "input_value",
+			      "name": "angle",
+			      "check": "Number"
+			    }
+			  ],
+				"inputsInline": true,
+			  "previousStatement": null,
+			  "nextStatement": null,
+			  "colour": Blockly.Blocks.bell_motion.HUE,
+			  "tooltip": Blockly.Msg.BELL_MOTION_SET_HSERVO_ANGLE_TOOLTIP,
+			  "helpUrl": Blockly.Msg.BELL_MOTION_SET_HSERVO_ANGLE_HELPURL
+			});
+	},
+
+	onchange: function (e) {
+		var warnText;
+		var block = this.getInputTargetBlock('angle');
+		if (!block) {
+			warnText = 'Must have an input block for `angle`.';
+		} else {
+			var angle = block.getFieldValue('NUM');
+			if (angle > 90 || angle < -90) {
+				warnText = 'angle must between -90 and 90';
+			}
+		}
+		this.setWarningText(warnText);
+	},
+};
+
+Blockly.Blocks['bell_get_hservo_angle'] = {
+	init: function () {
+		this.jsonInit(
+			{
+			  "type": "bell_get_hservo_angle",
+			  "message0": Blockly.Msg.BELL_MOTION_GET_HSERVO_ANGLE_TITLE,
+			  "args0": [
+			    {
+			      "type": "field_dropdown",
+			      "name": "sequence",
+			      "options": [
+			        [
+			          "1",
+			          "1"
+			        ],
+			        [
+			          "2",
+			          "2"
+			        ],
+			        [
+			          "3",
+			          "3"
+			        ],
+			        [
+			          "4",
+			          "4"
+			        ],
+			        [
+			          "5",
+			          "5"
+			        ],
+			        [
+			          "6",
+			          "6"
+			        ],
+			        [
+			          "7",
+			          "7"
+			        ],
+			        [
+			          "8",
+			          "8"
+			        ],
+			        [
+			          "9",
+			          "9"
+			        ],
+			        [
+			          "10",
+			          "10"
+			        ],
+			        [
+			          "11",
+			          "11"
+			        ],
+			        [
+			          "12",
+			          "12"
+			        ],
+			        [
+			          "13",
+			          "13"
+			        ],
+			        [
+			          "14",
+			          "14"
+			        ],
+			        [
+			          "15",
+			          "15"
+			        ]
+			      ]
+			    }
+			  ],
+			  "output": "Number",
+			  "colour": Blockly.Blocks.bell_motion.HUE,
+			  "tooltip": Blockly.Msg.BELL_MOTION_GET_HSERVO_ANGLE_TOOLTIP,
+			  "helpUrl": Blockly.Msg.BELL_MOTION_GET_HSERVO_ANGLE_HELPURL
+			});
+	},
+};
+
+Blockly.Blocks['bell_set_vservo_angle'] = {
+	init: function () {
+		this.jsonInit(
+			{
+			  "type": "bell_set_vservo_angle",
+			  "message0": Blockly.Msg.BELL_MOTION_SET_VSERVO_ANGLE_TITLE,
+			  "args0": [
+			    {
+			      "type": "field_dropdown",
+			      "name": "sequence",
+			      "options": [
+			        [
+			          "1",
+			          "1"
+			        ],
+			        [
+			          "2",
+			          "2"
+			        ],
+			        [
+			          "3",
+			          "3"
+			        ],
+			        [
+			          "4",
+			          "4"
+			        ],
+			        [
+			          "5",
+			          "5"
+			        ],
+			        [
+			          "6",
+			          "6"
+			        ],
+			        [
+			          "7",
+			          "7"
+			        ],
+			        [
+			          "8",
+			          "8"
+			        ],
+			        [
+			          "9",
+			          "9"
+			        ],
+			        [
+			          "10",
+			          "10"
+			        ],
+			        [
+			          "11",
+			          "11"
+			        ],
+			        [
+			          "12",
+			          "12"
+			        ],
+			        [
+			          "13",
+			          "13"
+			        ],
+			        [
+			          "14",
+			          "14"
+			        ],
+			        [
+			          "15",
+			          "15"
+			        ]
+			      ]
+			    },
+					{
+			      "type": "input_value",
+			      "name": "angle",
+			      "check": "Number"
+			    }
+			  ],
+				"inputsInline": true,
+			  "previousStatement": null,
+			  "nextStatement": null,
+			  "colour": Blockly.Blocks.bell_motion.HUE,
+			  "tooltip": Blockly.Msg.BELL_MOTION_SET_VSERVO_ANGLE_TOOLTIP,
+			  "helpUrl": Blockly.Msg.BELL_MOTION_SET_VSERVO_ANGLE_HELPURL
+			});
+	},
+
+	onchange: function (e) {
+		var warnText;
+		var block = this.getInputTargetBlock('angle');
+		if (!block) {
+			warnText = 'Must have an input block for `angle`.';
+		} else {
+			var angle = block.getFieldValue('NUM');
+			if (angle > 90 || angle < -90) {
+				warnText = 'angle must between -90 and 90';
+			}
+		}
+		this.setWarningText(warnText);
+	},
+};
+
+Blockly.Blocks['bell_get_vservo_angle'] = {
+	init: function () {
+		this.jsonInit(
+			{
+			  "type": "bell_get_vservo_angle",
+			  "message0": Blockly.Msg.BELL_MOTION_GET_VSERVO_ANGLE_TITLE,
+			  "args0": [
+			    {
+			      "type": "field_dropdown",
+			      "name": "sequence",
+			      "options": [
+			        [
+			          "1",
+			          "1"
+			        ],
+			        [
+			          "2",
+			          "2"
+			        ],
+			        [
+			          "3",
+			          "3"
+			        ],
+			        [
+			          "4",
+			          "4"
+			        ],
+			        [
+			          "5",
+			          "5"
+			        ],
+			        [
+			          "6",
+			          "6"
+			        ],
+			        [
+			          "7",
+			          "7"
+			        ],
+			        [
+			          "8",
+			          "8"
+			        ],
+			        [
+			          "9",
+			          "9"
+			        ],
+			        [
+			          "10",
+			          "10"
+			        ],
+			        [
+			          "11",
+			          "11"
+			        ],
+			        [
+			          "12",
+			          "12"
+			        ],
+			        [
+			          "13",
+			          "13"
+			        ],
+			        [
+			          "14",
+			          "14"
+			        ],
+			        [
+			          "15",
+			          "15"
+			        ]
+			      ]
+			    }
+			  ],
+			  "output": "Number",
+			  "colour": Blockly.Blocks.bell_motion.HUE,
+			  "tooltip": Blockly.Msg.BELL_MOTION_GET_VSERVO_ANGLE_TOOLTIP,
+			  "helpUrl": Blockly.Msg.BELL_MOTION_GET_VSERVO_ANGLE_HELPURL
+			});
+	},
 };
--- generators/lua/bell-extra.js
+++ generators/lua/bell-extra.js
@@ -3,18 +3,43 @@
 goog.provide('Blockly.Lua.bell_extra');
 goog.require('Blockly.Lua');
 
-Blockly.Lua['bell_extra_get_touch_state'] = function(block) {
-  var dropdown_sequence_name = block.getFieldValue('sequence_name');
-  // TODO: Assemble Lua into code variable.
-  var code = 'Get_touch_state(' + dropdown_sequence_name + ')';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Lua.ORDER_NONE];
+Blockly.Lua['bell_single_motor_light'] = function (block) {
+  var sequence = block.getFieldValue('motor');
+  var colorList = ['#f45d5d','#0eef53','#f6f550','#4d9edc','#d451f8','#07edfb','#f29450','#fcfcfb'];
+  var color = colorList.indexOf(block.getFieldValue('colour')) + 1;
+  var mode = block.getFieldValue('mode');
+  var code = `Set_motor_led(${sequence}, ${mode}, ${color})\n`;
+  return code;
 };
 
-Blockly.Lua['bell_extra_set_hservo_angle'] = function(block) {
-  var dropdown_sequence_name = block.getFieldValue('sequence_name');
-  var number_angle = block.getFieldValue('angle');
-  // TODO: Assemble Lua into code variable.
-  var code = 'Set_hservo_angle(' + dropdown_sequence_name + ', ' + number_angle + ')';
+Blockly.Lua['bell_touch_state'] = function (block) {
+  var sequence = block.getFieldValue('sequence');
+  var code = `(Get_touch_state(${sequence}) == 1)\n`;
+  return [code, Blockly.Lua.ORDER_ATOMIC];
+};
+
+Blockly.Lua['bell_set_hservo_angle'] = function (block) {
+  var sequence = block.getFieldValue('sequence');
+  var angle = Blockly.Lua.valueToCode(block, 'angle', Blockly.Lua.ORDER_ATOMIC) || 0;
+  var code = `Set_hservo_angle(${sequence}, ${angle} + 90)\n`;
   return code;
+};
+
+Blockly.Lua['bell_get_hservo_angle'] = function (block) {
+  var sequence = block.getFieldValue('sequence');
+  var code = `(Get_hservo_angle(${sequence}) - 90)\n`;
+  return [code,, Blockly.Lua.ORDER_ATOMIC];
+};
+
+Blockly.Lua['bell_set_vservo_angle'] = function (block) {
+  var sequence = block.getFieldValue('sequence');
+  var angle = Blockly.Lua.valueToCode(block, 'angle', Blockly.Lua.ORDER_ATOMIC) || 0;
+  var code = `Set_vservo_angle(${sequence}, ${angle} + 90)\n`;
+  return code;
+};
+
+Blockly.Lua['bell_get_vservo_angle'] = function (block) {
+  var sequence = block.getFieldValue('sequence');
+  var code = `(Get_vservo_angle(${sequence}) - 90)\n`;
+  return [code,, Blockly.Lua.ORDER_ATOMIC];
 };
